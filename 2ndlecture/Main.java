fc9b9c35ae715fc407cd29f14761ad94fc76f042
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-06 23:52:36 +0530

    variables and fundamentals of java

--- 2ndlecture/Main.java
+++ 2ndlecture/Main.java
@@ -1,5 +1,55 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.print("*\n**\n***\n****\n*****");
+//     }
+// }
+
+// public class Main {
+//     @SuppressWarnings("unused")
+//     public static void main(String[] args) {
+//         // Variable declaration
+//         String name = "sujal patel";
+//         int age = 19;
+//         double height = 5.6;
+//         boolean isStudent = true;
+//         double weight = 60.0;
+//         name = "payal";
+//         System.out.println(name);
+//     }
+// }
+// public class Main {
+//     @SuppressWarnings("unused")
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 20;
+//         int sum = a + b;
+//         System.out.println(sum);
+//         int diff = b - a;
+//         // System.out.println(diff);
+//         int mul = a * b;
+//         // System.out.println(mul);
+//         int div = b / a;
+//         // System.out.println(div);
+//     }
+// }
+
+import java.util.*;
+// public class Main {
+//     @SuppressWarnings("resource")
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+
 public class Main {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-        System.out.print("*\n**\n***\n****\n*****");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
