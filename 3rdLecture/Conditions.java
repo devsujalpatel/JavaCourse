d33a8bf11e405750ec4916e7c5a9062f718dd420
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-07 00:59:17 +0530

    conditions done

--- 3rdLecture/Conditions.java
+++ 3rdLecture/Conditions.java
@@ -5,12 +5,42 @@ public class Conditions {
     @SuppressWarnings("resource")
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-        if(age > 28){
-            System.out.println("You are a adult");
-        } 
-        else{
-            System.out.println("You are a child you can't learn java");
+        // int age = sc.nextInt();
+        // if(age > 28){
+        //     System.out.println("You are a adult");
+        // } 
+        // else{
+        //     System.out.println("You are a child you can't learn java");
+        // }
+
+        // int num = sc.nextInt();
+        // if(num % 2 == 0){
+        //     System.out.println("Even number");
+        // } else {
+        //     System.out.println("Odd number");
+        // }
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // if(a == b){
+        //     System.out.println("Equal");
+        // } else if (a > b) {
+        //         System.out.println("a is greater"); 
+        // } else {
+        //     System.out.println("a is lesser");
+        // }
+
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1 : System.out.println("hello");
+            break;
+            case 2 : System.out.println("Namaste");
+            break;
+            case 3 : System.out.println("Bonjour");
+            break;
+            default : System.out.println("Invalid Button");
         }
+
+       
     }   
 }
