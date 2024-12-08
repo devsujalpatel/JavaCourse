552fe487112f695ebc12bfcd22b2c5117a29321e
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-09 00:29:24 +0530

    assignment done

--- 5thLecture/Assignment.java
+++ 5thLecture/Assignment.java
@@ -3,14 +3,30 @@ public class Assignment {
     public static void main(String[] args) {
         int n = 5;
         int i, j;
-        for (i = 1; i <= n; i++){
-            for(j = 1; j <= n - i; j++){
-                System.out.print(" ");
-            }
-            for(j = 1; j <= n; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for (i = 1; i <= n; i++){
+        //     for(j = 1; j <= n - i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(j = 1; j <= n; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // Palindromic pyramid
+
+        // for(i = 1; i<=n; i++){
+        //     for(j=i; j<=n;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(j=1; j<=i;j++){
+        //         System.out.print(j+"");
+        //     }
+        //     for(j=i-1;j>=1;j--){
+        //         System.err.print(j+"");
+        //     }
+        //     System.out.println();
+        // }
+
+
     }
 }
