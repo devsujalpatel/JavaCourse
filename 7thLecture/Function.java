e6324f905b3398ef4517dbacd5194edebef4afc9
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-10 01:57:57 +0530

    factorial calculator

--- 7thLecture/Function.java
+++ 7thLecture/Function.java
@@ -6,9 +6,26 @@ public class Function{
     //     System.out.print(name);
     //     return;
     // }
-    public static int calculateSum(int a, int b){
-        int sum = a + b;
-        return sum;
+    // public static int calculateSum(int a, int b){
+    //     int sum = a + b;
+    //     return sum;
+    // }
+    // public static int calculateProduct(int a, int b){
+    //     return a * b;
+    // }
+
+    public static void calculateFactorial(int n){
+        int factorial = 1;
+        if(n<1){
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        for(int i=n; i>=1; i--){
+            factorial = factorial * i;
+        }   
+        System.err.println("The Factorial of the number "+n+" is:"+factorial);
+        return;
     }
     
 
@@ -16,11 +33,15 @@ public class Function{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // String name = sc.nextLine();
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
         
-        int sum = calculateSum(a, b);
-        System.out.println("Sum of Two Numbers: " + sum);
+        // int sum = calculateSum(a, b);
+        // System.out.println("Sum of Two Numbers: " + sum);
         // printMyName(name);
+        // System.out.println("Product of Two Numbers: " + calculateProduct(a, b));
+        int n = sc.nextInt();
+        calculateFactorial(n);
+    
     }
 }
