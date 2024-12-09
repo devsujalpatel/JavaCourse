7f0b3c83f32eb32a495f292001d5a3cb124dd656
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-10 00:53:15 +0530

    Diamond Pattern Solved

---
+++ 6thLecture/DiamondPatter.java
@@ -0,0 +1,25 @@
+public class DiamondPatter {
+    public static void main(String[] args) {
+        int i, j;
+        int n = 5;
+        for(i=1; i<=n; i++){
+            for(j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(i=n; i>=1; i--){
+            for(j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
