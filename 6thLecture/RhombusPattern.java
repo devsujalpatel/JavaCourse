a2c8eacc1f609ec459a7711cae5ce0b155ab37d5
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-09 23:00:21 +0530

    rhombus pattern printed

---
+++ 6thLecture/RhombusPattern.java
@@ -0,0 +1,15 @@
+public class RhombusPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+        for(i=1; i<=n; i++){
+            for(j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
