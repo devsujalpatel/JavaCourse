cfa4f78effa4a2540c3323a868fd79139eb7ae9c
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-10 01:48:08 +0530

    sum function

--- 7thLecture/Function.java
+++ 7thLecture/Function.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 
 public class Function{
 
-    public static void printMyName(String name){
-        System.out.print(name);
-        return;
+    // public static void printMyName(String name){
+    //     System.out.print(name);
+    //     return;
+    // }
+    public static int calculateSum(int a, int b){
+        int sum = a + b;
+        return sum;
     }
     
 
     @SuppressWarnings("resource")
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printMyName(name);
+        // String name = sc.nextLine();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = calculateSum(a, b);
+        System.out.println("Sum of Two Numbers: " + sum);
+        // printMyName(name);
     }
 }
