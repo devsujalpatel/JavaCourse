ea83c9b102b92545190d94c8ca1cf5ba9bd5bb30
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-10 21:04:01 +0530

    Seventh Question done

---
+++ 8thLecture-Exercise/SeventhQue.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SeventhQue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        String choice;
+
+        do {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+
+            if (number > 0) {
+                positiveCount++;
+            } else if (number < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+
+            System.out.print("Do you want to enter another number? (yes/no): ");
+            choice = scanner.next();
+        } while (choice.equalsIgnoreCase("yes"));
+
+        System.out.println("Count of positive numbers: " + positiveCount);
+        System.out.println("Count of negative numbers: " + negativeCount);
+        System.out.println("Count of zeros: " + zeroCount);
+
+        scanner.close();
+    }
+}
