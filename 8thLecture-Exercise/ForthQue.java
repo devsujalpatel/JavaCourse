c12b8b1bf2b06b00d192bb5b0443d5d757d37704
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-10 20:51:34 +0530

    4th Question done

--- 8thLecture-Exercise/ForthQue.java
+++ 8thLecture-Exercise/ForthQue.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class ForthQue {
     
+    public static void circumference(int r){
+        double c = 2*3.14*r;
+        System.out.println("Circumference of circle is "+c);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        circumference(r);
+    }
 }
