128f453151776f4f746f91d32e1f0832c3d881c8
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-12 23:10:02 +0530

    concatenation

--- 12thLecture/Strings.java
+++ 12thLecture/Strings.java
@@ -7,8 +7,14 @@ public class Strings {
         // String fullName = "Tony Stark";
         // String sentence = "My name is Tony Stark";
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Your name is " + name);
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("Your name is " + name);
+
+        // concatenation
+        String name = "Tony";
+        String surname = "Stark";
+        String fullName = name + " " + surname;
+        System.out.println("My name is " + fullName);
     }
 }
