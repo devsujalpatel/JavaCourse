ca08f8597d48271d3f0eb6529d68a6a911bed659
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-12 23:03:59 +0530

    String declaration

---
+++ 12thLecture/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        // Strings Declaration
+        String name = "Tony";
+        String fullName = "Tony Stark";
+        String sentence = "My name is Tony Stark";
+    }
+}
