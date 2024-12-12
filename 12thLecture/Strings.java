833e6916e3bc202318b5420e1a8c7914837ef681
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-12 23:09:16 +0530

    string inputs

--- 12thLecture/Strings.java
+++ 12thLecture/Strings.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Strings {
     public static void main(String[] args) {
         // Strings Declaration
-        String name = "Tony";
-        String fullName = "Tony Stark";
-        String sentence = "My name is Tony Stark";
+        // String name = "Tony";
+        // String fullName = "Tony Stark";
+        // String sentence = "My name is Tony Stark";
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is " + name);
     }
 }
