1a00a0760a5b8537e494aa6c5cee86f67ea16150
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-13 22:56:39 +0530

    learned set char at

---
+++ 13thLecture/StringBuilders.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class StringBuilders {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+        // char at index
+        System.out.println(sb.charAt(0));
+
+        // set char at index
+        // sb.setCharAt(0, 'P');
+        // System.out.println(sb);
+    }
+}
