4e9a0caceb5f4f9c8a8544347419d5f56b5431c3
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-13 23:41:44 +0530

    appending character in string at last using  append method

--- 13thLecture/StringBuilders.java
+++ 13thLecture/StringBuilders.java
@@ -2,21 +2,29 @@ import java.util.*;
 
 public class StringBuilders {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Tony");
-        System.out.println(sb);
+        // StringBuilder sb = new StringBuilder("Tony");
+        // System.out.println(sb);
         // char at index
-        System.out.println(sb.charAt(0));
+        // System.out.println(sb.charAt(0));
 
         // set char at index
         // sb.setCharAt(0, 'P');
         // System.out.println(sb);
 
         // insert char at index
-        sb.insert(2, 'n');
-        System.out.println(sb);
+        // sb.insert(2, 'n');
+        // System.out.println(sb);
 
-        // deleting
-        sb.delete(2, 4);
+        // // deleting
+        // sb.delete(2, 4);
+        // System.out.println(sb);
+        // appending new character at end
+        StringBuilder sb = new StringBuilder("h");
+
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
         System.out.println(sb);
     }
 }
