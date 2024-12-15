758e0f8a89d6f241d69c13cf049f37b3b52cae97
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-15 14:52:27 +0530

    arithmetic operators

--- 14thLecture/Operators.java
+++ 14thLecture/Operators.java
@@ -1,5 +1,25 @@
 public class Operators {
     public static void main(String[] args) {
-        
+        // Arithmetic Operators
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+        int d = a - b;
+        System.out.println(d);
+        int e = a * b;
+        System.out.println(e);
+        int f = a / b;
+        System.out.println(f);
+        int g = a % b;
+        System.out.println(g);
+        int h = a++;
+        System.out.println(h);
+        int i = a--;
+        System.out.println(i);
+        int j = ++a;
+        System.out.println(j);
+        int k = --a;
+        System.out.println(k);
     }
 }
