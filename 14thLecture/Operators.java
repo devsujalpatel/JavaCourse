7fb401c158adca4db3f9e64b9187d41eaee57a1a
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-15 15:02:58 +0530

    more operators

--- 14thLecture/Operators.java
+++ 14thLecture/Operators.java
@@ -3,25 +3,29 @@ public class Operators {
         // Arithmetic Operators
         int a = 10;
         int b = 20;
-        int c = a + b;
-        System.out.println(c);
-        int d = a - b;
-        System.out.println(d);
-        int e = a * b;
-        System.out.println(e);
-        int f = a / b;
-        System.out.println(f);
-        int g = a % b;
-        System.out.println(g);
-        // unary operators
-        int h = a++;
-        System.out.println(h);
-        int i = a--;
-        System.out.println(i);
-        int j = ++a;
-        System.out.println(j);
-        int k = --a;
-        System.out.println(k);
+        // int c = a + b;
+        // System.out.println(c);
+        // int d = a - b;
+        // System.out.println(d);
+        // int e = a * b;
+        // System.out.println(e);
+        // int f = a / b;
+        // System.out.println(f);
+        // int g = a % b;
+        // System.out.println(g);
+        // // unary operators
+        // int h = a++;
+        // System.out.println(h);
+        // int i = a--;
+        // System.out.println(i);
+        // int j = ++a;
+        // System.out.println(j);
+        // int k = --a;
+        // System.out.println(k);
+        // rational operators
+        
 
+        // logical operators
+        
     }
 }
