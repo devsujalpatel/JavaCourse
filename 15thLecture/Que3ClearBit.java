dd4e9c95c7899c49727a5740e34f2073b26c019b
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-30 00:20:42 +0530

    clear bit

--- 15thLecture/Que3ClearBit.java
+++ 15thLecture/Que3ClearBit.java
@@ -1,5 +1,11 @@
 public class Que3ClearBit {
     public static void main(String[] args) {
-        
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1 << pos;
+        int notBitMask = ~(bitMask);
+
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
     }
 }
