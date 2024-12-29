4cf35b91c50525eefcdf7ab124654354bee358bc
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-30 00:29:59 +0530

    update bit done

--- 15thLecture/Que4UpdateBit.java
+++ 15thLecture/Que4UpdateBit.java
@@ -1,7 +1,23 @@
+import java.util.*;
+
 public class Que4UpdateBit {
     public static void main(String[] args) {
-        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt();
+        int n = 5; // 0101
         int pos = 1;
-        int bitMask = 1 << pos;
+        int bitMask = 1<<pos;
+
+        if (oper == 1) {
+            // set
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        } else {
+            // clear
+            int newBitMask = ~(bitMask);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+        }
+
     }
 }
