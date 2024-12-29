0ae49d55471b53e7404169bb9afde871d03ee528
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-29 23:49:22 +0530

    set bit que

--- 15thLecture/BitManipulationQue2.java
+++ 15thLecture/BitManipulationQue2.java
@@ -1,5 +1,9 @@
 public class BitManipulationQue2 {
     public static void main(String[] args){
-        
+        int n = 5;
+        int pos = 1;
+        int bitMask = 1 << pos;
+        int newNumber = bitMask | n;
+        System.out.println(newNumber); 
     }
 }
