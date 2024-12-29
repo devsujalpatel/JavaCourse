1bb9dc20446f19ca6b89a7ba2cb5c40351aa68be
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-29 23:44:40 +0530

    bit manipulation

--- 15thLecture/BitManipulation.java
+++ 15thLecture/BitManipulation.java
@@ -1,5 +1,13 @@
 class BitManipulation{
     public static void main(String[] args) {
-        
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1 << pos;
+
+        if((bitMask & n) == 0) {
+            System.out.println("Bit was zero");
+        } else{
+            System.out.println("bit was one");
+        }
     }
 }
