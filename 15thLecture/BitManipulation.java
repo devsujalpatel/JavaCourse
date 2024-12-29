fba012f7bedc49241b551cc63a5816412005b41f
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-29 23:45:16 +0530

    position change

--- 15thLecture/BitManipulation.java
+++ 15thLecture/BitManipulation.java
@@ -1,7 +1,7 @@
 class BitManipulation{
     public static void main(String[] args) {
         int n = 5;
-        int pos = 2;
+        int pos = 3;
         int bitMask = 1 << pos;
 
         if((bitMask & n) == 0) {
